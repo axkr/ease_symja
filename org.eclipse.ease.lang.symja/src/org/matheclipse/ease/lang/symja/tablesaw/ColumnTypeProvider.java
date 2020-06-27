1f2d187f5cfb4264fb04d7d3a1baa7f27d689b3b
axexlck <axelclk@gmail.com>
2020-06-27 16:24:34 +0200

    misc

--- org.eclipse.ease.lang.symja/src/org/matheclipse/ease/lang/symja/tablesaw/ColumnTypeProvider.java
+++
@@ -1,8 +0,0 @@
-package org.matheclipse.ease.lang.symja.tablesaw;
-
-import tech.tablesaw.api.ColumnType;
-
-public interface ColumnTypeProvider {
-
-	public ColumnType getColumnType(int columnIndex);
-}
--- org.eclipse.ease.lang.symja/src/org/matheclipse/ease/lang/symja/tablesaw/TablesawEditorActionBarContributor.java
+++
@@ -1,7 +0,0 @@
-package org.matheclipse.ease.lang.symja.tablesaw;
-
-import org.eclipse.ui.part.EditorActionBarContributor;
-
-public class TablesawEditorActionBarContributor extends EditorActionBarContributor {
-
-}
